411d597be73cfededfa1fa0b633b1f3cbcfecd9e
GGWPx <shandy.sandoro.55.ss@gmail.com>
2024-12-29 00:08:26 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'GGWPx'
+description 'Critical Condition Notifications'
+version '2.5'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
